e0fcc7eb9cc5a3bb2504603d341c3c6c1c8f54e6
Sérgio M <sergio_tere@hotmail.com>
2020-07-16 11:16:46 -0300

    Prontinho

--- projeto-final/javaScript.js
+++ projeto-final/javaScript.js
@@ -1,22 +1,21 @@
 /*
 6.1variáveis e constantes, quero exemplos.
-    variáveis são espços na memorias que podem ser criados para armazenar um valor especificado pelo programador
+    variáveis são espaços criados na memorias que podem  armazenar um valor especificado pelo programador
     ex: var x = 3
     ex: var y = 'olá'
 
-    constantes são espços na memorias que podem ser criados para armazenar um valor especificado pelo programador e esses não
-    podem ser mutaveis
+    constantes são espaços não mutaveis criados na memorias que podem armazenar um valor especificado pelo programador
     ex: const a = "olá"
     ex const b = 3
 
 6.2comandos de decisão, quero exemplos.
-    comandos de decisão são aqueles em que o código deve saber se executa a tarefa ou não, por exemplo:
+    comandos de decisão são blocos de código que o algorítimo deve executa a tarefa ou não dependendo da condição, por exemplo:
     usando o if e else o código está decidindo se toma a decisão 1 caso a condição seja true ou a decisão 2 caso a condição seja false
     ir(0 == 0){"document.write("do this")"}
     else{"document.write("ops")"}
 
 6.3loops, quero exemplos.
-    os loops são comandos que repetem uma serie de comandos enquanto a condição estabelecida pelo programador for verdade.
+    os loops são comandos que repetem uma serie de outros comandos enquanto a condição estabelecida pelo programador for verdade.
     ex:
     while
     do while
@@ -32,6 +31,7 @@
 6.4funções, quero exemplos.
     funções são blocos de códios que podem ser criados e usados de acordo com a vontade e necessidade do programador, no momento em que a função é criada,
     o programador pode usar diversas vezes para que não precise ficar fazendo sempre o mesmo comando, ou grupo de comandos, desse jeito otimizando seu código.
+    a função possui o escolo local.
     ex:
     function doThis(){
         "document.write("do this")";
@@ -50,7 +50,7 @@
     }
     
 6.6 inputs e outputs, quero exemplos.
-    inputs são comandos que pertmitem que o usuário entre com alguma informação. ex: prompt()
+    inputs são comandos que permitem que o usuário entre com alguma informação. ex: prompt()
     outputs são comandos que permitem que a maquina se comunique com o usuário. ex: document.write()
 
 6.7array, quero exemplos.
@@ -62,14 +62,17 @@
     ex: x = {palavra: "olá", frita:'pera', idade: 39}
 
 6.9(camelCase, PascalCase, kibab-case, snake_case)
-    camelCase => a primeira letra minúscula e as demais palavras possum suas primeiras letras maiúscola e sem espaço usado como boas práticas.
-    PascalCase => cada palavra possui a primeira letra maiúscola usado como boas práticas.
-    kibab-case => a primeira letra maiúscola e as outras palavras são separadas por um ifem usado como boas práticas.
-    snake_case => todas as letras minúscolas e as palavras são separadas po um underline.
+    camelCase => a primeira letra minúscula e as demais palavras possum suas primeiras letras maiúscula e sem espaço usado como boas práticas.
+    PascalCase => cada palavra possui a primeira letra maiúscula usado como boas práticas.
+    kibab-case => a primeira letra maiúscula e as outras palavras são separadas por um ifem usado como boas práticas.
+    snake_case => todas as letras minúsculas e as palavras são separadas po um underline.
 
 6.10Diferença entre caminho absoluto e caminho relativo
-    caminho relativo são aqueles em que o código pode retornar valores diferentes dependendo do caso.
-    caminho absoluto são aqueles que sempre devem retornar o mesmo resultado.
+    caminho relativo: o programa reconhece aquele caminho de diretórios somente na maquina, ou pasta, expecifica, em uma outra maquina o programa iria ficar perdido entro os diretórios
+    ex: C:\Users\Usuario\Desktop\Provinha Legal\projeto-final-nivelamento\projeto-final
+
+    caminho absoluto: o programa reconhece o caminho a partir de um ponto especifico, isse permite que ele consiga encontrar tudo o que precisa independendente do diretório que ele esteja.
+    ex: ./../LiCENSE
 
 6.11Diferença entre os operadores &&, ||, == e != 
     && representa o e no js
@@ -78,15 +81,16 @@
     != representa o diferente, comparador de diferença no js
 
 6.12O que é EPER, e para que serve.
-    Entender, Plandejar, Executar, Revisar. serve para que o programador permanessa focado e saiba o que está fazendo , evitando que se perca em seu raciocínio lógico durante a programação
+    Entender, Planejar, Executar, Revisar. serve para que o programador permaneça focado e saiba o que está fazendo , evitando que se perca em seu raciocínio lógico 
+    durante a programação
 
 .13Explique o que é Pilha e Fila, quero exemplos. 
-    fila => primeiro item a entrar em uma fila, é o primeiro item a sair dela
-    pilha => primeiro item a entrar em uma pilha é o ultimo item a sair dela
+    fila => primeiro item a entrar em uma fila, é o primeiro item a sair dela =FIFA
+    pilha => primeiro item a entrar em uma pilha é o ultimo item a sair dela = FILO
 
 6.14Explique com suas palavras qual a diferença entre html, javascript e css.
-    html é a parte que estrutura uma webpage e que conecta js e css
+    html é a parte que estrutura uma webpage, contento todos os campos e algumas interaçãos
     css é a parte em que torna tudo bonito e convidativo para o usuário
     js é a parte operacional que toma decisões e torna as coisas funcionais
-    
+
 */
